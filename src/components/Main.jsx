4c110b81dcee682781530c50218b567b3b55e9ad
Allyson Mitchem <allysonmitchem@gmail.com>
2023-11-20 11:16:22 -0700

    created navbar component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'   
+import Navbar from './Navbar';
 
 const Main = () => {
     return(
         <div id ="main">
+            <Navbar/>
+            <h2> Apple</h2>
 
         </div>
     )
